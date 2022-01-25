3feeb7c5cccc30a79e518b03a9c50b216f159897
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2022-01-25 17:05:01 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var table = document.getElementsByClassName("wallet_history_table")[0];
+for (i = 2; i < table.tBodies[0].rows.length; i++){
+    var cell = table.getElementsByClassName("wht_total ")[i];
+    console.log(cell);
+    //var inside_cell = cell.querySelectorAll("div");
+}
