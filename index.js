8edefbb1dbe80404cccca7952b5904ee0294bee3
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2022-01-26 10:35:14 -0300

    valora total de cada linha
    obtendo o valor total de cada linha

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var table = document.getElementsByClassName("wallet_history_table")[0];
 for (i = 2; i < table.tBodies[0].rows.length; i++){
     var cell = table.getElementsByClassName("wht_total ")[i];
-    console.log(cell);
-    //var inside_cell = cell.querySelectorAll("div");
+    
+    var inside_cell = cell.querySelectorAll("div");
+    if(inside_cell.length === 2 || inside_cell.length === 1){
+        console.log(inside_cell.item(0).innerText.replace(/[^0-9\,]/g, ''));
+    }else {
+        console.log(cell.innerText.replace(/[^0-9\,]/g, ''));
+    }
 }
