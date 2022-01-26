644b65778fd993ebc3fb6875a8f040fe9ff9e25f
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2022-01-26 11:58:37 -0300

    entrada saída
    Informa quanto dinheiro entrou e saiu da conta e a diferença entrada - saída

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var table = document.getElementsByClassName("wallet_history_table")[0];
 var total_geral = 0.0;
+var entrada = 0.0;
+var saida = 0.0;
 for (i = 2; i < table.tBodies[0].rows.length; i++){
     var cell = table.getElementsByClassName("wht_total ")[i];
     
@@ -7,21 +9,23 @@ for (i = 2; i < table.tBodies[0].rows.length; i++){
     if(inside_cell.length === 2){
         var str = inside_cell.item(0).innerText.replace(/[^0-9\,]/g, '');
         str = str.replace(/,/g, '.');
-        total_geral = total_geral + parseFloat(str);
-        console.log(str);
+        entrada = entrada + parseFloat(str);
+        //console.log(str);
         //console.log(inside_cell.item(0).innerText.replace(/[^0-9\,]/g, ''));
     }else if( inside_cell.length === 1){
         var str = inside_cell.item(0).innerText.replace(/[^0-9\,]/g, '');
         str = str.replace(/,/g, '.');
-        total_geral = total_geral - parseFloat(str);
-        console.log(str);
+        saida = saida + parseFloat(str);
+        //console.log(str);
         //console.log(inside_cell.item(0).innerText.replace(/[^0-9\,]/g, ''));
     }else{
         var str = cell.innerText.replace(/[^0-9\,]/g, '');
-         str = str.replace(/,/g, '.');
-        total_geral = total_geral - parseFloat(str);
-        console.log(str);
+        str = str.replace(/,/g, '.');
+        saida = saida + parseFloat(str);
+        //console.log(str);
         //console.log(cell.innerText.replace(/[^0-9\,]/g, ''));
     }
 }
-console.log(total_geral);
+console.log("Saida: R$ ", saida.toFixed(2));
+console.log("Entrada: R$ ", entrada.toFixed(2));
+console.log("Diferença: R$ ", (entrada - saida).toFixed(2));
